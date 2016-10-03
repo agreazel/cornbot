52215737469bb06549d4f8c410c5b97ee6b278f3
Ann Greazel <ann.greazel@gmail.com>
2016-10-03 10:10:24 -0500

    Update test.cornbot.rb

--- cornbot/test.cornbot.rb
+++ cornbot/test.cornbot.rb
@@ -47,7 +47,7 @@ class Date
       when 101..401 then :winter
       when 402..630 then :spring
       when 701..930 then :summer
-      when 1001..1231 then :fall
+      when 931..1231 then :fall
     end
   end
 end
